4581105bfbd73895383c1a80356eb005732ba6c7
Ian <iang74908@gmail.com>
2025-02-24 12:05:27 +0000

    borrado de 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Version 1");
-        System.out.println("Version 2");
+
     }
 }
